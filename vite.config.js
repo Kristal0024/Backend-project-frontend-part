c10483682ddadd3f25522d0b427cb5ade88775ac
Kristal Shrestha <kristalstha20@gmail.com>
2026-02-17 19:21:01 +0545

    debugged vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"Backend-project-frontend-part",
+  base:"/Backend-project-frontend-part/",
   plugins: [react()],
 })
