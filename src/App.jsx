1f315d508c4f2a8a58935ddf7f87e35e19b5a1f9
Kristal Shrestha <kristalstha20@gmail.com>
2026-02-17 19:28:27 +0545

    deploying problem solved:

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import {HashRouter as Router,Routes,Route} from 'react-router-dom'
 import CreatePost from './pages/CreatePost'
 import Feed from './pages/Feed'
 import Home from './pages/Home'
